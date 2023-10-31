380404f9ce20debb60358506040082c3ddc6c656
Brem090 <brem090@gmail.com>
2023-11-01 00:18:34 +0200

    Validation_update
    new validation

--- models/Email.py
+++ models/Email.py
@@ -1,4 +1,8 @@
 from models.Field import Field
 
 class Email(Field):
-    pass
+    def __init__(self, value):
+        if "@" not in value or "." not in value.split("@")[-1] or " " in value:
+            raise ValueError("invalid email address.")
+        super.__init__(value)
+    
