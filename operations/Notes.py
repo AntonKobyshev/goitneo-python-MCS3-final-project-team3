82c33bff523f24dceeae66e50735f699805541d2
Anton Kobyshev <antonkobyshev@gmail.com>
2023-10-31 21:00:55 +0200

    Update Notes.py

---
+++ operations/Notes.py
@@ -0,0 +1,15 @@
+class Notes(UserDict):
+    def add_note(self, note):
+        pass
+
+    def find(self, name):
+        pass
+
+    def delete(self, name):
+        pass
+
+    def save(self):
+        pass
+
+    def load(self, filename='notebook.bin'):
+        pass
