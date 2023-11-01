5785108164f8859ff1129a22f8cb8ba5868fe205
Anton Kobyshev <antonkobyshev@gmail.com>
2023-11-01 22:01:51 +0200

    update

--- models/Name.py
+++ models/Name.py
@@ -1,9 +1,14 @@
+import re
 from models.Field import Field
 
 
 class Name(Field):
     def __init__(self, value):
-        if not value.isalpha() or not value:
+        if not self.is_valid_name(value):
             raise ValueError(
                 "Name must contain only letters and cannot be empty")
         super().__init__(value)
+
+    def is_valid_name(self, name):
+        pattern = r"^[a-zA-Z\s]+$"
+        return bool(re.match(pattern, name))
